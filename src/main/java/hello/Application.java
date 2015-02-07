1a1639b2d5e1282df143d5db893a521f60c3bcbc
Carlus Henry <carlushenry@gmail.com>
2015-02-07 15:30:13 -0500

    will now scan the new objects for dependency injection candidates.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
+@ComponentScan(basePackages = {"hello", "com.ioextendedgr.web"})
 @EnableAutoConfiguration
 public class Application {
 
